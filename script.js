f8f80db9eb5be0c27fa0892a7fd4e284465bb270
Key Feula <keyfeula1214@gmail.com>
2025-05-26 22:06:11 -0700

    Add boilerplate html and fetch function in script to get pokemon data

---
+++ script.js
@@ -0,0 +1,8 @@
+fetch("https://pokeapi.co/api/v2/pokemon/ditto", {mode: "cors"})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error Code: " + response.status);
+        }
+        return response.json();
+    })
+    .then(response => console.log(response));
